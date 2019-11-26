7ed0b8a82168f0bd3130c62fba8500d55ba7f738
Leonardo Felipe dos Santos <42083917+leosan95@users.noreply.github.com>
2019-11-25 22:51:38 -0300

    att pc

--- Organizar/criarpastas.py
+++ Organizar/criarpastas.py
@@ -15,6 +15,9 @@ maximo = nomesloop.count()+1
 for i in range(1,maximo):
     nome = nomesloop[:i:]
 
+pastaOrganiza = './organização/'
+os.makedirs(pastaOrganiza)
+
 path = './contas'
 for x in nome:
     dir = path+'/'+x
